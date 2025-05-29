a37e0255fe07fd7ef46f7cc8ed1a8ceac9aae8fd
Andre Augusto <andre.augusto@tecnico.ulisboa.pt>
2025-05-29 01:25:08 +0100

    Added working demo
    Signed-off-by: Andre Augusto <andre.augusto@tecnico.ulisboa.pt>

--- gateway/oracle/case_3/oracle-evm-execute-update.py
+++ gateway/oracle/case_3/oracle-evm-execute-update.py
@@ -1,6 +1,7 @@
 
 import requests
 import json
+from time import sleep
 
 TEST_DATA = "DATA WRITTEN TO THE BLOCKCHAIN"
 DATA_HASH = "0xd2a21947eed980d6266fd60e26f24379032c4fa65ed8c63b323e040ea2b57536"
@@ -38,7 +39,7 @@ def execute_update(file_path):
         params = json.load(file)
 
     req_params = {
-        'destinationNetworkId': { 'id': 'EthereumLedgerTestNetwork', 'ledgerType': 'ETHEREUM' },
+        'destinationNetworkId': { 'id': 'HardhatTestNetwork1', 'ledgerType': 'ETHEREUM' },
         'destinationContract': {
             "contractAbi": params["abi"],
             "contractName": params["contractName"],
@@ -52,35 +53,6 @@ def execute_update(file_path):
 
     return execute_oracle(req_params)
 
-def execute_read(file_path):
-    """
-    Calls the /api/v1/@hyperledger/cactus-plugin-satp-hermes/oracle/execute endpoint
-    with the given file as JSON body.
-
-    Args:
-        file_path (str): The path to the JSON file to send.
-
-    Returns:
-        dict: The JSON response from the endpoint.
-    """
-    with open(file_path, 'r') as file:
-        params = json.load(file)
-
-    req_params = {
-        'sourceNetworkId': { 'id': 'EthereumLedgerTestNetwork', 'ledgerType': 'ETHEREUM' },
-        'sourceContract': {
-            "contractAbi": params["abi"],
-            "contractName": params["contractName"],
-            "contractBytecode": params["bytecode"],
-            "contractAddress": "0x5FbDB2315678afecb367f032d93F642f64180aa3",
-            "methodName": "getData",
-            "params": [DATA_HASH]
-        },
-        'taskType': 'READ'
-    }
-
-    return execute_oracle(req_params)
-
-
-print(execute_update("../artifacts/contracts/OracleTestContract.sol/OracleTestContract.json"))
-print(execute_read("../artifacts/contracts/OracleTestContract.sol/OracleTestContract.json"))
+if __name__ == "__main__":
+    update_response = execute_update("../../../EVM/artifacts/contracts/OracleTestContract.sol/OracleTestContract.json")
+    print("Response:", update_response)
